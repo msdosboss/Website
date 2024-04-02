d632893306752c5a75eb9d367c0a70bbf4976f79
orcalord <jacobt0722@gmail.com>
2024-04-02 13:10:56 -0700

    php bare test

--- executeOrder66.php
+++ executeOrder66.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['execute'])){
-    $output = shell_exec('echo Execute Order 66 >> ~/test')
+    //$output = shell_exec('echo Execute Order 66 >> ~/test')
 
     echo "Order 66 complete"
 }

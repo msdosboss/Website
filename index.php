b8a2ac69a48636a32a5235e1fbcfd6b108656cd7
orcalord <jacobt0722@gmail.com>
2024-04-03 08:29:39 -0700

    php shell_exec

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 if (isset($_POST['execute'])){
-    //$output = shell_exec('echo Execute Order 66 >> ~/test')
+    $output = shell_exec('echo Execute Order 66 >> /srv/http/website/test');
 
-    echo "Order 66 complete"
+    echo "Order 66 complete";
 }
 
 ?>

cddacde15d2d784dcf17321e7500d3e29619792d
msdosboss <jacobt0722@gmail.com>
2024-04-03 09:37:33 -0700

    testing safemode

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
-<?php
-if(function_exists('exec')) {
-    echo "exec is enabled";
+<?php 
+// Check for safe mode
+if( ini_get('safe_mode') ){
+    // Do it the safe mode way
+echo "Do it the safe mode way";
+}else{
+    // Do it the regular way
+echo "Do it the regular way";
 }
-    phpinfo();
+
 ?>

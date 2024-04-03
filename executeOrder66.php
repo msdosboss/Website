8f95dd1f541d9e77843301589be40e297b6d3631
msdosboss <jacobt0722@gmail.com>
2024-04-03 10:03:49 -0700

    removed echo

--- executeOrder66.php
+++ executeOrder66.php
@@ -2,7 +2,7 @@
 if (isset($_POST['execute'])){
     $output = shell_exec('echo Execute Order 66 >> /srv/http/website/test');
 
-    echo "Order 66 complete";
+    //echo "Order 66 complete";
 }
 
 ?>

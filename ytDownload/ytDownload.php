8ed4aacb8ca58ffb7e3bc439d3f21d0a5990a8db
msdosboss <jacobt0722@gmail.com>
2024-04-04 10:32:23 -0700

    file download

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -8,6 +8,20 @@
     $url = $_POST['URL'];
     $format = $_POST['formats'];
     $output = shell_exec("echo $url with optition $format >> /srv/http/website/test2");
+
+    $file = '/srv/http/website/test2';
+
+    if(file_exists($file)){
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename($file).'"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        readfile($file);
+        exit;
+    }
     //echo "This is $a";
 
 ?>

f789b8cf64dc4117b42c725a63a108deaf5f63a2
orcalord <jacobt0722@gmail.com>
2024-04-04 13:52:47 -0700

    mp3 download maybe

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -7,10 +7,18 @@
 
     $format = $_POST['formats'];
     $url = isset($_POST['URL']) ? escapeshellarg($_POST['URL']) : '';
-    $output = shell_exec("echo $url with option $format >> /srv/http/website/test2");
+    //$output = shell_exec("echo $url with option $format >> /srv/http/website/test2");
 
-    $file = '/srv/http/website/test2';
+    if($format == 'mp3'){
+        $outputTemplate = "/srv/http/website/videos/%(title)s.%(ext)s";
+        $output = shell_exec("yt-dlp --extract-audio --audio-format mp3 -o '$outputTemplate' $url");
+        $file = shell_exec("yt-dlp --get-filename -o '$outputTemplate' $url");
+    }
+    else if($format == 'mp4'){
 
+    }
+    
+    
     if(file_exists($file)){
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');

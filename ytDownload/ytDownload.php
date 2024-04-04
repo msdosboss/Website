aa0e4f03641518b20b4d45bf4959a72ce5848163
msdosboss <jacobt0722@gmail.com>
2024-04-04 09:30:47 -0700

    test

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -1,10 +1,11 @@
 <?php
-if (isset($_POST['execute'])){
-    //$output = shell_exec('echo Execute Order 66 >> /srv/http/website/test');
+/*if (isset($_POST['execute'])){
+    $output = shell_exec('echo Execute Order 66 >> /srv/http/website/test');
 
-    //echo "Order 66 complete";
+    echo "Order 66 complete";
+}*/
 
-    echo $_POST['URL'];
-}
+echo $_POST['URL'];
+$output = shell_exec('echo Execute Order 66 >> /srv/http/website/test2')
 
 ?>

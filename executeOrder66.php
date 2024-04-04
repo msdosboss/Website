39ac477f40ce97510b1bed7c85e117bac7645eec
msdosboss <jacobt0722@gmail.com>
2024-04-04 08:51:23 -0700

    added user input to php file

--- executeOrder66.php
+++ executeOrder66.php
@@ -1,8 +1,12 @@
 <?php
 if (isset($_POST['execute'])){
-    $output = shell_exec('echo Execute Order 66 >> /srv/http/website/test');
+    //$output = shell_exec('echo Execute Order 66 >> /srv/http/website/test');
 
     //echo "Order 66 complete";
 }
+
+$a = readline('Enter a Youtube link');
+
+echo $a;
 
 ?>

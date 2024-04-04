87e4717001a467235e1aeb75bc8e15d57029a237
msdosboss <jacobt0722@gmail.com>
2024-04-04 09:46:20 -0700

    whoops2

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -1,12 +1,12 @@
 <?php
-    if (isset($_POST['execute'])){
+    /*if (isset($_POST['execute'])){
         $output = shell_exec('echo Execute Order 66 >> /srv/http/website/test');
 
         echo "Order 66 complete";
-    }
+    }*/
 
-    /*$a = $_POST['URL'];
+    $a = $_POST['URL'];
     $output = shell_exec('echo ('$a') >> /srv/http/website/test2');
-    echo "This is ('$a')";*/
+    echo "This is ('$a')";
 
 ?>

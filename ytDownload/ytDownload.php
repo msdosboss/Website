c80d55bbe32a964abdb85047502132fe52322922
msdosboss <jacobt0722@gmail.com>
2024-04-04 09:34:52 -0700

    trying to put user input in file

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -1,11 +1,12 @@
 <?php
-/*if (isset($_POST['execute'])){
-    $output = shell_exec('echo Execute Order 66 >> /srv/http/website/test');
+    /*if (isset($_POST['execute'])){
+        $output = shell_exec('echo Execute Order 66 >> /srv/http/website/test');
 
-    echo "Order 66 complete";
-}*/
+        echo "Order 66 complete";
+    }*/
 
-echo $_POST['URL'];
-$output = shell_exec('echo Execute Order 66 >> /srv/http/website/test2')
+    $a = $_POST['URL'];
+    $output = shell_exec('echo ('$a') >> /srv/http/website/test2');
+    echo "This is ('$a')";
 
 ?>

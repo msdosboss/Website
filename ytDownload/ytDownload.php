88018d3cfaf02dfba41b8861da63df67c80c9c8d
orcalord <jacobt0722@gmail.com>
2024-04-05 10:09:11 -0700

    typo

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -11,8 +11,8 @@
     //$output = shell_exec("echo $url with option $format >> /srv/http/website/test2");
     $outputTemplate = "/srv/http/website/videos/%(title)s.%(ext)s";
     if(substr_compare($url, "https://www.youtube.com", 0, 23) != 0){
-        echo "Not valid input"
-        exit
+        echo "Not valid input";
+        exit;
     }
     if($format == 'mp3'){        
         $output = shell_exec("yt-dlp --extract-audio -o '$outputTemplate' '$url'"); //--audio-format mp3

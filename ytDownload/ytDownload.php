ebf5dc7452bbc5d0403679793794f571cb566a11
msdosboss <jacobt0722@gmail.com>
2024-04-04 19:01:50 -0700

    Maybe download will work

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -27,7 +27,7 @@
     
     if(file_exists($file)){
         header('Content-Description: File Transfer');
-        header('Content-Type: video/webm');
+        header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename="'.basename($file).'"');
         header('Expires: 0');
         header('Cache-Control: must-revalidate');

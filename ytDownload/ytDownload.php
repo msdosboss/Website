332fa24ae19cdbe2179dcbf7c9bdc4e766c65705
msdosboss <jacobt0722@gmail.com>
2024-04-04 17:18:32 -0700

    removed %(title)s.%(ext)s from dir

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -10,9 +10,9 @@
     //$output = shell_exec("echo $url with option $format >> /srv/http/website/test2");
 
     if($format == 'mp3'){
-        $outputTemplate = "/srv/http/website/videos/"; //%(title)s.%(ext)s
+        $outputTemplate = "/srv/http/website/videos/audio.mp3"; //%(title)s.%(ext)s
         $output = shell_exec("yt-dlp --extract-audio -o '$outputTemplate' '$url'"); //--audio-format mp3
-        $file = shell_exec("yt-dlp --get-filename -o '$outputTemplate' $url");
+        $file = "/srv/http/website/videos/audio.mp3"
     }
     else if($format == 'mp4'){
 

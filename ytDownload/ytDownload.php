84591e5fe4e00800b63ae0714e471370cd0fb461
msdosboss <jacobt0722@gmail.com>
2024-04-04 18:38:58 -0700

    Maybe download will work

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -17,7 +17,7 @@
         $file = str_replace(" .", ".", $file);
         //$file = str_replace(".webm", ".mp3", $file);
         $file = "/srv/http/website/videos/'" . $file;
-        //$file = $file . "'";
+        $file = trim($file) . "'";
         echo $file;
     }
     else if($format == 'mp4'){

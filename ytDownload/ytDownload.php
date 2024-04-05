fd588c483c9c6a176a787f1c3a9f5752df21961c
orcalord <jacobt0722@gmail.com>
2024-04-05 10:15:09 -0700

    debugging

--- ytDownload/ytDownload.php
+++ ytDownload/ytDownload.php
@@ -10,7 +10,7 @@
     $url = filter_var($url, FILTER_SANITIZE_URL);
     //$output = shell_exec("echo $url with option $format >> /srv/http/website/test2");
     $outputTemplate = "/srv/http/website/videos/%(title)s.%(ext)s";
-    if(substr_compare($url, "https://www.youtube.com", 0) != 0){
+    if(substr_compare($url, "'https://www.youtube.com", 0, 24) != 0){
         echo "Not valid input $url";
         exit;
     }

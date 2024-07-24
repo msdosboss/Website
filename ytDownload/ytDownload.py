6ec4adb21b2183f685371c3b99ec0bb9d1b43ecc
msdosboss <jacobt0722@gmail.com>
2024-07-24 15:50:31 -0700

    audio files are now being downloaded as m4a so I needed to change the py file

--- ytDownload/ytDownload.py
+++ ytDownload/ytDownload.py
@@ -3,15 +3,16 @@ import os
 import sys
 
 if(sys.argv[1] == "-a"):
-    list_of_files = glob.glob('/srv/http/website/videos/*.webm')
+	list_of_files = glob.glob('/srv/http/website/videos/*.webm')
 elif(sys.argv[1] == "-v"):
-    list_of_files = glob.glob('/srv/http/website/videos/*.mp4')
+	list_of_files = glob.glob('/srv/http/website/videos/*.mp4')
 latest_file = max(list_of_files, key=os.path.getctime)
 latest_file_new = latest_file
 latest_file_new = latest_file_new.replace(" ", "")
 latest_file_new = latest_file_new.replace("'", "")
 
 if(sys.argv[1] == "-a"):
-    latest_file_new = latest_file_new.replace(".webm", ".mp3")
+	latest_file_new = latest_file_new.replace(".webm", ".mp3")
+	latest_file_new = latest_file_new.replace(".m4a", ".mp3")
 
 os.rename(latest_file, latest_file_new)
